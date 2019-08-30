40cfcace25f9fbc190c3be5d1bf2766390ef523f
Dmitriy109 <froll_d@mail.ru>
2019-08-30 15:47:07 +0300

    HW 4 lesson

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -3,7 +3,8 @@
 # 1 или более символов в верхнем регистре.
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
-
+import re
+import random
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
        'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'\
@@ -20,6 +21,27 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
+# '''
+# С регуляркой
+# '''
+# west = r"[a-z]+"
+# westside = re.findall(west, line)
+# print(westside)
+# '''
+# Без регулярки
+# '''
+symbols = (list(map(chr, range(ord('A'), ord('Z')+1))))
+new_line = list(line)
+
+for i, symb in enumerate(new_line[:]):
+       for symb2 in symbols:
+              if symb == symb2:
+                     new_line[i] = ' '
+
+str_split = ''.join(new_line).split(' ')
+
+line_str = [i for i in str_split if i != '']
+print(line_str)
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
@@ -45,9 +67,28 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+'''
+С регуляркой
+'''
+west = r"[a-z]{2}([A-Z]+)[A-Z]{2}"
+westside = re.findall(west, line_2)
+print(westside)
+'''
+Без регулярки
+'''
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+# with open("11111.txt", "w+") as file:
+#        _do_file = ("w+", ''.join(str(i) for i in [random.randint(1, 9) for x in range(2500)]))
+#
+# def find_longest_rep(_do_file):
+#     result = max(re.findall(r'((\d+?)\2+)', _do_file), key=lambda t: len(t[0]))
+#     return result[0]
+#
+# print(find_longest_rep(_do_file[1]))
